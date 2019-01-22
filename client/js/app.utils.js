31d0fa44d97d16daa54eeee0651efeb03939abc4
Robert Munn <robert.d.munn@gmail.com>
2019-01-21 18:29:28 -0700

    - remove old client files

--- client/js/app.utils.js
+++
@@ -1,6 +0,0 @@
-app.utils = {
-  formatDate: function( dateString ){
-    var date = new Date( dateString );
-    return ( date.getMonth() + 1 ) + "/" + date.getDate() + "/" + date.getFullYear();
-  }
-}
